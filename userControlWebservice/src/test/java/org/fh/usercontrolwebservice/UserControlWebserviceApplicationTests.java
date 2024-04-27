9f80dd932c3d36e2741e8096b62ba89ff58b7b6b
Unger Daniel <2210859021@fh-burgenland.at>
2024-04-28 01:51:28 +0200

    feat: introduce usercontrolwebservice class from inventorymanagement project

---
+++ userControlWebservice/src/test/java/org/fh/usercontrolwebservice/UserControlWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fh.usercontrolwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserControlWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
