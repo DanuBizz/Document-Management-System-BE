1bb4192325bf90c13d4ee6530b4f02bd7894fcd1
Unger Daniel <2210859021@fh-burgenland.at>
2024-04-28 04:47:22 +0200

    feat: add api-gateway from inventory management project

--- userControlWebservice/src/main/java/org/fh/usercontrolwebservice/configuration/WebSecurityConfig.java
+++ userControlWebservice/src/main/java/org/fh/usercontrolwebservice/configuration/WebSecurityConfig.java
@@ -61,8 +61,8 @@ public class WebSecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         //configuration.setAllowedOrigins(Arrays.asList("http://example.com"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        //configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        //configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowedOrigins(Collections.emptyList());
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
