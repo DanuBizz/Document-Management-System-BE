f5f4be4cbbf6525d78a02a3936ef8b28a5dc0def
Unger Daniel <2210859021@fh-burgenland.at>
2024-04-28 02:43:06 +0200

    feat: adapt docker-compose.yml to create userauth db and application.properties

--- userControlWebservice/src/main/java/org/fh/usercontrolwebservice/configuration/WebSecurityConfig.java
+++ userControlWebservice/src/main/java/org/fh/usercontrolwebservice/configuration/WebSecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 /**
@@ -60,8 +61,8 @@ public class WebSecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         //configuration.setAllowedOrigins(Arrays.asList("http://example.com"));
-        //configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-        //configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowedOrigins(Collections.emptyList());
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
