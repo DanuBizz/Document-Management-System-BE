c9e83cdfae203cae1bb469bd1bab6bdf903a52a2
Hasnat-Sajid <2210859008@fh-burgenland.at>
2024-04-29 18:28:18 +0200

    feat: fixed a bug in swagger

---
+++ documentManagementService/src/main/java/org/fh/documentmanagementservice/exception/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package org.fh.documentmanagementservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException() {
+        super("Resource not found");
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
