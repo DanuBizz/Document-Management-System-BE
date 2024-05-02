aeda0d8d8ad45ab5523aab09921762204b49cbf6
mike <mike_sirb@yahoo.de>
2024-05-02 17:58:22 +0200

    ADD: new endpoint to fetch latest document with associated versions

--- documentManagementService/src/main/java/org/fh/documentmanagementservice/documentVersion/DocumentVersionRepository.java
+++ documentManagementService/src/main/java/org/fh/documentmanagementservice/documentVersion/DocumentVersionRepository.java
@@ -1,6 +1,11 @@
 package org.fh.documentmanagementservice.documentVersion;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DocumentVersionRepository extends JpaRepository<DocumentVersion, Long> {
+    Page<DocumentVersion> findByIsLatestTrue(Pageable pageable);
+
+    Page<DocumentVersion> findByDocumentNameAndIsLatestFalse(Pageable pageable, String documentName);
 }
