f2af456e241ea7aa66706a8a58e778cff3e04ef3
Hasnat-Sajid <2210859008@fh-burgenland.at>
2024-05-05 13:39:37 +0200

    feat: added the pop db script

--- init.sql
+++ init.sql
@@ -76,16 +76,6 @@ INSERT INTO documents (name) VALUES
                                  ('Sales Data Analysis'),
                                  ('Product Launch Plan');
 
--- Insert test data for document_versions
-INSERT INTO document_versions (document_id, timestamp, file_path, is_read, is_latest, is_visible) VALUES
-                                                                                                      (1, '2024-01-01 12:00:00', '/documents/proposal_v1.docx', FALSE, TRUE, TRUE),
-                                                                                                      (1, '2024-01-02 12:00:00', '/documents/proposal_v2.docx', TRUE, FALSE, TRUE),
-                                                                                                      (2, '2024-02-01 14:30:00', '/documents/report_v1.pdf', FALSE, TRUE, TRUE),
-                                                                                                      (2, '2024-02-02 15:00:00', '/documents/report_v2.pdf', TRUE, FALSE, TRUE),
-                                                                                                      (3, '2024-03-01 09:00:00', '/documents/minutes_v1.txt', FALSE, TRUE, TRUE),
-                                                                                                      (4, '2024-03-02 11:00:00', '/documents/sales_v1.xlsx', TRUE, TRUE, TRUE),
-                                                                                                      (5, '2024-04-01 10:00:00', '/documents/launch_plan_v1.pptx', FALSE, TRUE, TRUE);
-
 -- Insert test data for categories
 INSERT INTO categories (name) VALUES
                                   ('Finance'),
@@ -104,12 +94,3 @@ INSERT INTO user_category (category_id, user_id) VALUES
                                                      (4, 6),
                                                      (5, 7);
 
--- Insert test data for user_documents
-INSERT INTO user_documents (user_id, document_version_id, has_read) VALUES
-                                                                        (1, 1, FALSE),
-                                                                        (2, 2, TRUE),
-                                                                        (3, 3, FALSE),
-                                                                        (4, 4, TRUE),
-                                                                        (5, 5, FALSE),
-                                                                        (6, 6, TRUE),
-                                                                        (7, 7, FALSE);
