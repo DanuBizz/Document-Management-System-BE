adf71322e8ed398ca36a9219034fd1a12eb47b68
Hasnat-Sajid <2210859008@fh-burgenland.at>
2024-05-28 14:22:59 +0200

    feat: added an endpoint for the documentRead

--- documentManagementService/src/main/java/org/fh/documentmanagementservice/userDocumentRead/UserDocumentReadService.java
+++ documentManagementService/src/main/java/org/fh/documentmanagementservice/userDocumentRead/UserDocumentReadService.java
@@ -32,8 +32,10 @@ public class UserDocumentReadService {
      */
     @Transactional
     public UserDocumentReadResponseDTO markDocumentAsRead(UserDocumentReadRequestDTO request) {
-        User user = userRepository.findById(request.getUserId()).orElseThrow(() -> new RuntimeException("User not found"));
-        DocumentVersion documentVersion = documentVersionRepository.findById(request.getDocumentVersionId()).orElseThrow(() -> new RuntimeException("Document version not found"));
+        User user = userRepository.findById(request.getUserId())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        DocumentVersion documentVersion = documentVersionRepository.findById(request.getDocumentVersionId())
+                .orElseThrow(() -> new RuntimeException("Document version not found"));
 
         UserDocumentRead userDocumentRead = new UserDocumentRead();
         userDocumentRead.setUser(user);
